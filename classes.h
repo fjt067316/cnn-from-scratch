6f4b99eb4e40c3b7d910a5534b63ec682c64d5f2
Josh <joshuatfolkins@gmail.com>
2023-04-17 21:51:34 -0400

    adam working kind of for softmax

--- classes.h
+++ classes.h
@@ -12,6 +12,9 @@ void printArray(vector<double> arr, int size) {
     cout << endl;
 }
 
+class AdamFCL;
+
+
 void print_vector(const vector<vector<vector<double>>>& vec) {
     cout << "[";
     for (const auto& v1 : vec) {
@@ -102,6 +105,157 @@ vector<double> min_max_scale(vector<double> input) {
     return input;
 }
 
+
+/*
+class Adam{
+    // https://arxiv.org/pdf/1412.6980.pdf
+    // https://towardsdatascience.com/how-to-implement-an-adam-optimizer-from-scratch-76e7b217f1cc
+public:
+    double m_dw, v_dw, m_db, v_db, beta1, beta2, epsilon, learning_rate;
+
+    Adam(double learning_rate=0.01, double beta1=0.9, double beta2=0.999, double epsilon=1e-8) {
+        m_dw = v_dw = m_db = v_db = 0;
+        this->beta1 = beta1;
+        this->beta2 = beta2;
+        this->epsilon = epsilon;
+        this->learning_rate = learning_rate;
+    }
+
+    std::pair<double, double> update(int t, double w, double b, double dw, double db) {
+        // dw, db are from the current minibatch
+        // momentum beta 1
+        // weights
+        m_dw = beta1 * m_dw + (1 - beta1) * dw; // biased momentum estimate
+        // biases
+        m_db = beta1 * m_db + (1 - beta1) * db; 
+
+        // rms beta 2
+        // weights
+        v_dw = beta2 * v_dw + (1 - beta2) * pow(dw, 2); // bias corrected momentum estimate
+        // biases
+        v_db = beta2 * v_db + (1 - beta2) * pow(db, 2);
+
+        // bias correction
+        double m_dw_corr = m_dw / (1 - pow(beta1, t));
+        double m_db_corr = m_db / (1 - pow(beta1, t));
+        double v_dw_corr = v_dw / (1 - pow(beta2, t));
+        double v_db_corr = v_db / (1 - pow(beta2, t));
+
+        // update weights and biases
+        w = w - learning_rate * (m_dw_corr / (sqrt(v_dw_corr) + epsilon));
+        b = b - learning_rate * (m_db_corr / (sqrt(v_db_corr) + epsilon));
+
+        return std::make_pair(w, b);
+    }
+};
+*/
+
+class AdamFCL{
+public:
+    vector<vector<double>>  m_dw, v_dw;
+    vector<double> m_db, v_db;
+    double beta1, beta2, epsilon, learning_rate, initial_b1, initial_b2;
+    int t = 0;
+    int counter=0;
+    int iterations = 100;
+    double decay_rate = 0.8;
+    double gamma_init = 0.00001;
+    // rows = dw.size() cols = dw[0].size()
+
+    AdamFCL(double rows, double cols, double learning_rate=0.0005, double beta1=0.9, double beta2=0.999, double epsilon=1e-8) :
+    m_dw(rows, vector<double>(cols, 0)),
+    v_dw(rows, vector<double>(cols, 0)),
+    m_db(rows, 0),
+    v_db(rows, 0)
+    {
+       
+        this->beta1 = beta1;
+        this->initial_b1 = beta1;
+        this->initial_b2 = beta2;
+        this->beta2 = beta2;
+        this->epsilon = epsilon;
+        this->learning_rate = learning_rate;
+    }
+
+    void update( vector<vector<double>> *w, vector<double> *b, vector<vector<double>> dw, vector<double> db) { // t is current timestep
+        // dw, db are what we would usually update params with gradient descent
+        // printArray(m_dw[0], 10);
+        // printArray(dw[0], 10);
+        // counter++;
+        // if((counter % iterations) == 0){
+        //     // beta1 *= initial_b1;
+        //     // beta2 *= initial_b2;
+        // }
+        this->t++;
+
+        // momentum beta 1
+        // weights
+        for(int i=0; i< (*w).size(); i++){
+            for(int j=0; j< (*w)[0].size(); j++){
+                m_dw[i][j] = beta1 * m_dw[i][j] + (1 - beta1) * dw[i][j]; // biased momentum estimate
+                v_dw[i][j] = beta2 * v_dw[i][j] + (1 - beta2) * pow(dw[i][j], 2); // bias corrected momentum estimate
+            }
+        }
+                // printArray(m_dw[0], 10);
+
+        // cout << m_dw[7][7] << endl;
+        // print_vector(m_dw);
+        // biases
+        for(int i=0; i< (*w).size(); i++){
+            m_db[i] = beta1 * m_db[i] + (1 - beta1) * db[i]; 
+            v_db[i] = beta2 * v_db[i] + (1 - beta2) * pow(db[i], 2);
+        }
+        
+
+        // rms beta 2
+        // weights
+        // biases
+        vector<vector<double>> m_dw_corr(m_dw.size(), vector<double>(m_dw[0].size()));
+        vector<vector<double>> v_dw_corr(m_dw.size(), vector<double>(m_dw[0].size()));
+        vector<double> m_db_corr(m_dw.size());
+        vector<double> v_db_corr(m_dw.size());
+
+        double denom_mw = (1 - pow(beta1, t));
+        double denom_vw = (1 - pow(beta2, t));
+        // cout << denom_mw << endl;
+        for (int i = 0; i < m_dw.size(); i++) {
+            for (int j = 0; j < m_dw[i].size(); j++) {
+                m_dw_corr[i][j] = m_dw[i][j] / denom_mw;
+                v_dw_corr[i][j] = v_dw[i][j] / denom_vw;
+            }
+        }
+
+        // bias correction
+        double denom_mb = (1 - pow(beta1, t));
+        double denom_vb = (1 - pow(beta2, t));
+
+        for (int j = 0; j < m_dw.size(); j++) {
+            m_db_corr[j] = m_db[j] / denom_mb;
+            v_db_corr[j] = v_db[j] / denom_vb;
+        }
+
+        // printArray(m_dw_corr[0], 10);
+        // update weights and biases 
+        // double gamma = gamma_init * decay_rate;
+        for(int i=0; i< (*w).size(); i++){
+            for(int j=0; j< (*w)[i].size(); j++){
+
+                (*w)[i][j] -= learning_rate * (m_dw_corr[i][j] / (sqrt(v_dw_corr[i][j]) + epsilon));
+            }
+        }
+        // printArray(m_db_corr, 10);
+        // learning_rate = learning_rate * sqrt(denom_vb) / denom_mb;
+
+        // b -= learning_rate *  dL/dZ
+        for(int i=0; i < (*b).size(); i++){
+            (*b)[i] -=learning_rate * (m_db_corr[i] / (sqrt(v_db_corr[i]) + epsilon)); 
+        }
+        
+        return;
+    }
+};
+
+
 class Softmax {
 public:
 vector<double> last_input;
@@ -131,6 +285,7 @@ vector<double> last_input;
 
     vector<double> backwards(vector<double> dLdZ){ // cross_entropy dLdZ = -1/p
     // https://github.com/AlessandroSaviolo/CNN-from-Scratch/blob/master/src/layer.py
+    // https://victorzhou.com/blog/intro-to-cnns-part-2/
         vector<double> dLdZ_exp(dLdZ.size(), 0.0);
         vector<double> dout_dt(dLdZ.size(), 0.0); // dout_dt is dLdZ next layer
         double sum_exp = 0.0;
@@ -162,7 +317,7 @@ vector<double> last_input;
 void set_mask(vector<vector<double>> *mask) {
     random_device rd;
     mt19937 gen(rd());
-    binomial_distribution<> d(1, 0.5);
+    binomial_distribution<> d(1, 0.9);
 
     for(int i = 0; i < mask->size(); i++) {
         for (int j = 0; j < (*mask)[0].size(); j++) {
@@ -259,7 +414,7 @@ public:
     int input_depth;
     // vector<vector<vector<double>>> bias; // bias is added to 3d result after apply every filter
     vector<double> bias; // dB is calcaulted by averaging dLdZ
-    ConvolutionLayer(int num_filters, int input_depth, int input_rows, int input_cols, int filter_len, int stride = 1, bool padding=0) {
+    ConvolutionLayer(int num_filters, int input_depth, int filter_len, int stride = 1, bool padding=0) {
         this->num_filters = num_filters;
         this->filter_len = filter_len;
         this->stride = stride;
@@ -579,23 +734,30 @@ public:
     vector<vector<double>> weights; // matrix of shape (output_size, input_size)
     vector<double> bias; // vector of shape (output_size)
     vector<double> input_matrix;
+    // AdamFCL* adam;
+    AdamFCL adam;
 
-    FullyConnectedLayer(int input_size, int output_size) {
+
+    FullyConnectedLayer(int input_size, int output_size) :
+        input_size(input_size),
+        output_size(output_size),
+        weights(output_size, vector<double>(input_size)),
+        bias(output_size, 0.0),
+        adam(output_size, input_size)
+    {
         this->input_size = input_size;
         this->output_size = output_size;
+        // adam = new AdamFCL(output_size, input_size);
 
         // Initialize weights with random values
         random_device rd;
         mt19937 gen(rd());
         normal_distribution<double> dist(0.0, sqrt(2.0 / input_size));
 
-        this->weights.resize(output_size, vector<double>(input_size));
         for (auto& row : this->weights) {
             generate(row.begin(), row.end(), [&](){ return dist(gen); });
         }
 
-        // Initialize bias with zeros
-        this->bias.resize(output_size, 0.0);
     }
 
     vector<double> forward(vector<double> input_matrix, bool dropout=false) {
@@ -651,17 +813,19 @@ public:
                 dLdW[r][c] = dLdZ[r]*input_matrix[c];
             }
         }
-        // w -= learning_rate * dL/dW
-        for(int i=0; i< weights.size(); i++){
-            for(int j=0; j< weights[i].size(); j++){
-                weights[i][j] -= learning_rate * dLdW[i][j];
-            }
-        }
-        
-        // b -= learning_rate *  dL/dZ
-        for(int i=0; i<bias.size(); i++){
-            bias[i] -= learning_rate* 10 * dLdZ[i]; // make learning rate for bias larger because its smaller number smaller condition more likely to convergeto 0
-        }
+
+        adam.update(&weights, &bias, dLdW, dLdZ);
+
+        // // w -= learning_rate * dL/dW
+        // for(int i=0; i< weights.size(); i++){
+        //     for(int j=0; j< weights[i].size(); j++){
+        //         weights[i][j] -= learning_rate * dLdW[i][j];
+        //     }
+        // }
+        // // b -= learning_rate *  dL/dZ
+        // for(int i=0; i<bias.size(); i++){
+        //     bias[i] -= learning_rate* 10 * dLdZ[i]; // make learning rate for bias larger because its smaller number smaller condition more likely to convergeto 0
+        // }
         // printArray(bias, 10);
 
         return next_dLdZ;
@@ -947,8 +1111,3 @@ void dropout3d(double probability, vector<vector<vector<double>>>& inputVector)
         }
     }
 }
-
-
-
-
-
